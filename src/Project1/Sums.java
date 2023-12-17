64fd0373c4b9b21e18b4514b3f023af95df30a77
manas <manaszdn@gmail.com>
2023-12-17 13:03:56 +0530

    My first commit

---
+++ src/Project1/Sums.java
@@ -0,0 +1,13 @@
+package Project1;
+
+public class Sums {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20;
+		int b=36;
+		int h=a+b;
+		System.out.println(h);
+	}
+
+}
